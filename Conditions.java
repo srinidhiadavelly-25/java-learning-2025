e95f5c29a32256b484d6f2a862aa9cf2c4459b92
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2025-12-22 23:53:25 +0530

    practiced conditions

---
+++ Conditions.java
@@ -0,0 +1,16 @@
+public class Conditions{
+    public static void main (String[]args){
+        /*int num = 8;
+        if(num > 0){
+            System.out.println("The number is positive number:" + num);
+        }
+        int num = 101;
+        if(num > 100){
+            System.out.println("The number is greater than 100:" + num);
+        }*/
+        int num = 25;
+        if(num % 5 == 0){
+            System.out.println("The number is divisible by 5:" + num);
+        }
+    }
+}
--- Operators.java
+++ Operators.java
@@ -1,5 +1,7 @@
 public class Operators {
     public static void main(String[] args){
+    //if-condition
+
     /*int a = 10;
     int b = 20;
     int add = a + b;
@@ -16,10 +18,93 @@ public class Operators {
     System.out.println("a < b: " + (a < b));
     System.out.println("a > b: " + (a > b));
     System.out.println("a == b: " + (a == b));
-    System.out.println("a != b: " + (a != b));*/
+    System.out.println("a != b: " + (a != b));
 
     int a = 20;
     System.out.println("Pre increment: " + (++a));
     System.out.println("post increment: " + (a++));
+    
+    //if-else condition
+
+    int num = 7;
+    if(num % 2 == 0){
+        System.out.println("The number is even number");
+    }
+    else{
+         System.out.println("The number is odd number");
+    }
+
+   int age = 24;
+   if(age >= 18){
+    System.out.println("Your are eligible to vote");
+   }
+   else{
+    System.out.println("Your are not eligible");
+   }
+
+  int num1 = 25;
+  int num2 = 25;
+  if(num1 < num2){
+    System.out.println("num2 is the largest");
+  }
+  else if (num1 > num2) {
+    System.out.println("num1 is the largest");
+  } else {
+    System.out.println("Both are same");
+  }
+
+    //Swtich condition
+
+    int day = 8;
+    switch (day) {
+        case 1:
+            System.out.println("It's Monday");
+            break;
+        case 2:
+            System.out.println("It's Tuesday");
+            break;
+        case 3:
+            System.out.println("It's Wednesday");
+            break;
+        case 4:
+            System.out.println("It's Thursday");
+            break;
+        case 5:
+            System.out.println("It's Friday");
+            break;
+        case 6:
+            System.out.println("It's saturday");
+            break;
+        case 7:
+            System.out.println("It's sunday");
+            break;
+        default:
+            System.out.println("we have only 7 days in a week");
+            break;
+    }
+
+    int a = 10;
+    int b = 20;
+    int Calculator = 5;
+    switch (Calculator){
+        case 1:
+            System.out.println(" Addition: " + (a+b));
+            break;
+        case 2:
+            System.out.println("Substraction: " + (a-b));
+            break;
+        case 3:
+            System.out.println("Multiplication: " + (a*b));
+            break;
+        case 4:
+            System.out.println(" Division: " + (a/b));
+            break;
+        case 5:
+            System.out.println("Modulus: " + (a%b));
+            break;
+        default:
+            break;
+    }*/
+   
 }
 }
