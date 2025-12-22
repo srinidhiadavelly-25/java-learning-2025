88bbf257e486f1896be958af5e71230568f759e6
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2025-12-22 22:39:10 +0530

    practiced data types

---
+++ Datatypes.java
@@ -0,0 +1,26 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        /*String name = "Srinidhi";
+        int age = 23;
+        double height = 5.5;
+        char grade = 'A';
+        boolean fitness = true;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Fitness: " + fitness);
+
+        int a = 3;
+        double b = a;
+        System.out.println("int value: " + a);
+        System.out.println("double value: " + b);*/
+        
+        int fitness = true;
+        boolean age = 23;
+        System.out.println("fitness: " + fitness);
+        System.out.println("age: " + age);
+
+    }
+    
+}
