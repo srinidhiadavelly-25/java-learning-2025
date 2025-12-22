15abf8204e93422db37d99a7e84b6af5ccd4151b
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2025-12-22 22:58:50 +0530

    practiced operators

---
+++ Operators.java
@@ -0,0 +1,25 @@
+public class Operators {
+    public static void main(String[] args){
+    /*int a = 10;
+    int b = 20;
+    int add = a + b;
+    int sub = a - b;
+    int multi = a * b;
+    double div = (double)a / b;
+    System.out.println("Add: " + add);
+    System.out.println("Substraction: " + sub);
+    System.out.println("Multiply: " + multi);
+    System.out.println(" Divide: " + div);
+
+    int a = 15;
+    int b = 20;
+    System.out.println("a < b: " + (a < b));
+    System.out.println("a > b: " + (a > b));
+    System.out.println("a == b: " + (a == b));
+    System.out.println("a != b: " + (a != b));*/
+
+    int a = 20;
+    System.out.println("Pre increment: " + (++a));
+    System.out.println("post increment: " + (a++));
+}
+}
