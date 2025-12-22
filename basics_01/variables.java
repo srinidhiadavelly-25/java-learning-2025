50519707e0e0bd415c6c3d69a60263f2ef57f8bf
srinidhiadavelly-25 <srinidhiadavelly@gmail.com>
2025-12-22 11:49:13 +0530

    practiced variables programs

---
+++ basics_01/variables.java
@@ -0,0 +1,31 @@
+package basics_01;
+
+public class variables {
+    public static void main(String[] args) {
+        /*int age = 23;
+        double height = 5.5;
+        String name = "Srinidhi";
+        System.out.println("Age:" + age);
+        System.out.println("Height:" + height);
+        System.out.println("Name: " + name);
+        int a = 5;
+        int b = 4;
+        int sum = a+b;
+        int diff = a-b;
+        int product = a*b;
+        double quotient = (double) a/b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(product);
+        System.out.println(quotient);*/
+        int a = 5;
+        System.out.println("Initial value: " + a);
+        a = 6;
+        System.out.println("1st updated value: " + a);
+        a = 7;
+        System.out.println("2nd updated value: " + a);
+        a = 8;
+        System.out.println("3rd updated value: " + a);
+    }
+    
+}
